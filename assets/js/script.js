4740c2845ad105da48f1a5139b2a19e3695bdbfd
Anh Pham <quanganh1202@gmail.com>
2019-11-01 01:46:19 +0700

    Init project

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+function openContactForm() {
+    document.getElementById("contact-panel").style.width = "300px";
+}
+
+function closeContactForm() {
+    document.getElementById("contact-panel").style.width = "0px";
+}
+
+function onScrollPage(event) {
+    const contentElement = document.querySelector('html');
+    const headerElement = document.querySelector('#header');
+    if (headerElement && contentElement.scrollTop > 0) {
+        headerElement.style.background = "rgba(0, 0, 0, 0.5)";
+    } else {
+        headerElement.style.background = "transparent";
+    }
+}
+
+function checkPageScroll() {
+    document.addEventListener('scroll', onScrollPage);
+}
+
+function onClickTitle(element) {
+    if (element.parentElement.classList.contains('show-detail')) {
+        element.parentElement.classList.remove('show-detail');
+    } else {
+        document.querySelectorAll('.info-title').forEach(function (element) {
+            element.classList.remove('show-detail');
+        })
+        element.parentElement.focus();
+        element.parentElement.classList.add('show-detail');
+    }
+}
