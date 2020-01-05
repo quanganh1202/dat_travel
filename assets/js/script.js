97de8381d28f333a5a9ff879f81e90872dddb65d
Anh Pham <quanganh1202@gmail.com>
2020-01-06 00:29:33 +0700

    Update UI

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,3 +42,48 @@ function showLocationMark(locationMarkId, isShowLocationMark) {
         else mark.style.display = 'none';
     }
 }
+
+function viewDayDetail(day) {
+    const modal = $('#schedule-detailed-modal');
+    const modalHeader = $('#schedule-detailed-label');
+    const controlLeft = $('#day-control-left');
+    const controlRight = $('#day-control-right');
+    var headerText = '';
+    if (day === 1) {
+        controlLeft.addClass('hidden-control');
+        controlRight.removeClass('hidden-control');
+        headerText = 'Giorno 1 : Arrivo ad Hanoi';
+    }
+    if (day === 2) {
+        controlLeft.removeClass('hidden-control');
+        controlRight.removeClass('hidden-control');
+        headerText = 'Giorno 2 : Visita’ di Hanoi';
+    }
+    if (day === 3) {
+        controlLeft.removeClass('hidden-control');
+        controlRight.removeClass('hidden-control');
+        headerText = 'Giorno 3 : Hanoi – baia di Halong';
+    }
+    if (day === 4) {
+        controlLeft.removeClass('hidden-control');
+        controlRight.removeClass('hidden-control');
+        headerText = 'Giorno 4 : Halong – Hanoi – Volo per Hoi An';
+    }
+    if (day === 5) {
+        controlLeft.removeClass('hidden-control');
+        controlRight.addClass('hidden-control');
+        headerText = 'Giorno 5 : Visita di Hoi An';
+    }
+    modal.data('day', day);
+    modalHeader.text(headerText);
+}
+
+function goToDayDetailByOneStep(goBy) {
+    const modal = $('#schedule-detailed-modal');
+    const currentModalDay = modal.data().day;
+    if (goBy === 'prev') {
+        viewDayDetail(currentModalDay - 1);
+    } else {
+        viewDayDetail(currentModalDay + 1);
+    }
+}
